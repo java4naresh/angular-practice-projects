155492968ec1c6e6171ac935df43964fffe50750
java4naresh <kambalanaresh4java@gmail.com>
2020-03-14 12:06:32 +0530

    added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: 'forms/profile-form.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { StudentComponent } from './student/student.component';
+
+const routes:Routes = [
+  { path:'employee', component:EmployeeComponent},
+    
+   {path:'student', component: StudentComponent, },
+   {path:'', redirectTo:'/employee', pathMatch:'full' }
+ 
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
